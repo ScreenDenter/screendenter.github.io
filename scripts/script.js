e49c8c364f83e54cdded9f4f8155abb487c0608a
Iskander <60399353+PrytexY@users.noreply.github.com>
2020-05-31 22:49:51 +0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+width=screen.width; // ширина  
+height=screen.height; // высота
+
+if (width<600){
+	location.href = 'mobile';
+}
+
+if (width>=600 && width<1400){
+	location.href = 'http://www.google.com/';
+
+
+}
